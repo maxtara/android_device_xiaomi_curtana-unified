8bf777a4dd57f78972cdb3b86b2848cbb7bce067
LeddaZ <leonardoledda@gmail.com>
2021-01-16 14:39:30 +0100

    Revert "curtana: build correct light hal"
    This reverts commit 5737b5a30d3de0954ce25498f9caf7896379aeb1.

--- device.mk
+++ device.mk
@@ -333,7 +333,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sm6250
+    android.hardware.light@2.0-service.xiaomi_atoll
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
